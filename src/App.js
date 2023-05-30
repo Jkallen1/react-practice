796e9fa345ab234d39d2e039dd68fec319c90f85
Joshua Allen <Jayallen11@outlook.com>
2023-05-30 19:27:49 -0400

    created const variable

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, {
   useState
 } from "react";
 
+
+
 function App() {
+  const [count, setCount] = useState(4)
   return ( <
     >
     <
     button > - < /button> <
-    span > 0 < /span> <
+    span > {
+      count
+    } < /span> <
     button > + < /button>
 
     <
